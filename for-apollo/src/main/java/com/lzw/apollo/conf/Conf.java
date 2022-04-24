f50081efcfdd869a4d56051f5dab994cff1eb1bb
liuzhiwei365 <liuzhiwei365@sina.com>
2022-04-24 13:48:56 +0800

    first

---
+++ for-apollo/src/main/java/com/lzw/apollo/conf/Conf.java
@@ -0,0 +1,12 @@
+package com.lzw.apollo.conf;
+
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.context.annotation.Configuration;
+
+@EnableApolloConfig
+@Configuration
+public class Conf {
+
+
+}
